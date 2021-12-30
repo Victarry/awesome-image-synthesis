ac417fef880300a055d745dfe0130836af68e505
Zhenhuan Liu <nkulzh16@gmail.com>
2021-12-30 21:29:49 +0800

    Fix bug.

--- .github/actions/update_readme.py
+++ .github/actions/update_readme.py
@@ -5,7 +5,7 @@ sch = SemanticScholar(timeout=2)
 p = re.compile('https://arxiv.org/abs/\d+\.\d+')
 
 lines = []
-with open('../../README.md', 'r') as f:
+with open('./README.md', 'r') as f:
     for line in f.readlines():
         m = p.search(line)
         if m:
@@ -20,6 +20,6 @@ with open('../../README.md', 'r') as f:
         print(line, end="")
         lines.append(line)
 
-with open('../../README.md', 'w') as f:
+with open('./README.md', 'w') as f:
     for line in lines:
         f.write(line)
